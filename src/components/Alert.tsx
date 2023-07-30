d2d98907cc69c98d2bb096f1e2b72d10449e978a
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2023-07-30 20:33:17 +0000

    using dates

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -6,15 +6,17 @@ type AlertProps = {
 }
 
 export const Alert = (props: AlertProps) => {
+   const { type, heading, children } = props;
+   let today: Date = new Date;
    return (
       <div>
          <div>
             <span role="img"
-               aria-label={props.type === "warning" ? "Warning" : "Information"}>
-               {props.type === "warning" ? "⚠️" : "i"}</span>
-            <span>{props.heading}</span>
+               aria-label={type === "warning" ? "Warning" : "Information"}>
+               {type === "warning" ? "⚠️" : "i"}</span>
+            <span>{heading}</span>
          </div>
-         <div>{props.children}.</div>
+         <div>{children} date: {today.toDateString()}</div>
       </div>
    )
 }
