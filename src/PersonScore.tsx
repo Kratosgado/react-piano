2cf7504e1c3bd3c8e591abfc460f5721dfe84568
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2023-07-31 00:57:47 +0000

    finish useState hook

--- src/PersonScore.tsx
+++ src/PersonScore.tsx
@@ -1,10 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getPerson } from "./getPerson";
 
 export const PersonScore = () => {
+   const [name, setName] = useState<string | undefined>();
+   const [score, setScore] = useState(0);
+   const [loading, setLoading] = useState(true);
+
    useEffect(() => {
-      getPerson().then((person) => console.log(person))
+      getPerson().then((person) => {
+         setLoading(false);
+         setName(person.name);
+      })
    }, []);
 
-   return null
+   if (loading) {
+      return <div>Loading ...</div>
+   }
+   return (
+      <div>
+         <h3>
+            {name}, {score}
+         </h3>
+         <button onClick={()=>setScore(score+1)}>Add</button>
+         <button onClick={()=>setScore(score-1)}>Subtract</button>
+         <button onClick={()=>setScore(0)}>Reset</button>
+      </div>
+   )
 }
